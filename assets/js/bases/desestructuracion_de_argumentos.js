eea2790a7e3198986c2efcf38cc1aa5a55cc6bf4
Boris Garcés <borisgarces@imac-de-boris.home>
2023-08-26 21:28:16 +0200

    act

--- assets/js/bases/desestructuracion_de_argumentos.js
+++ assets/js/bases/desestructuracion_de_argumentos.js
@@ -1,7 +1,8 @@
 // La "desestructuración de argumentos" en JavaScript es una técnica que te permite extraer valores de un objeto o un array directamente como parámetros en una función. Esto puede hacer que tu código sea más limpio y legible al evitar la necesidad de acceder a propiedades específicas de objetos o elementos de un array dentro de la función.
 
-// Aquí tienes un ejemplo de cómo se ve la desestructuración de argumentos en JavaScript:
-// Usando objetos
+// En este ejemplo, la función imprimirDatosUsuario toma un objeto como argumento y utiliza la desestructuración para extraer las propiedades nombre y edad directamente. La función imprimirFrutas toma un array como argumento y desestructura los elementos para obtener las dos primeras frutas.
+
+// Esto puede ser particularmente útil cuando trabajas con funciones que tienen muchos argumentos o cuando deseas evitar el acceso repetitivo a propiedades o elementos de arrays dentro de la función. Ten en cuenta que la desestructuración de argumentos también te permite proporcionar valores predeterminados en caso de que alguna propiedad o elemento no esté presente en el objeto o array que estás desestructurando.
 
 function imprimirDatosUsuario({ nombre, edad }) {
   console.log(`Nombre: ${nombre}, Edad: ${edad}`);
